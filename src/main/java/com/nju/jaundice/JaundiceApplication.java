4ea6afb9450c77d48751aa07ca0ce61b102473f8
zp2946062178 <2799712178@qq.com>
2018-12-26 14:52:26 +0800

    init

---
+++ src/main/java/com/nju/jaundice/JaundiceApplication.java
@@ -0,0 +1,14 @@
+package com.nju.jaundice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaundiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JaundiceApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/nju/jaundice/JaundiceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.nju.jaundice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JaundiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
